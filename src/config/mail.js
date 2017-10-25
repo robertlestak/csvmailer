b0437584ec1461a297804c92036590cf5ca24ce4
adal.io <hello@adal.io>
2017-10-24 17:00:39 -0700

    Pooled sending

--- src/config/mail.js
+++ src/config/mail.js
@@ -3,6 +3,9 @@ module.exports = {
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
   secure: true,
+  pool: true,
+  maxConnections: 5,
+  maxMessages: Infinity,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
